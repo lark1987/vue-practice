dd2f96463b5a5c3fd00c142db4cbf433a3a57a66
lark1987 <bosworth210920@gmail.com>
2024-04-16 16:36:28 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HomeTopbar from './components/home-topbar.vue'
+</script>
+
+<template>
+  <HomeTopbar />
+
+  <nav></nav>
+
+  <main></main>
+
+  <RouterLink to="/">Home</RouterLink>
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/components/home-topbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>我是header，好辛苦啊 QQ</header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    return {}
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
