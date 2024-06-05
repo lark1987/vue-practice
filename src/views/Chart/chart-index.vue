d21fbb98d2ff62dab6786de93a68c45d57b93396
lark1987 <bosworth210920@gmail.com>
2024-06-05 18:04:41 +0800

    Update the pie chart.

--- src/views/Chart/chart-index.vue
+++ src/views/Chart/chart-index.vue
@@ -14,7 +14,7 @@ import {
 import { UniversalTransition } from 'echarts/features'
 import { CanvasRenderer } from 'echarts/renderers'
 import VChart, { THEME_KEY } from 'vue-echarts'
-import { ref, provide } from 'vue'
+import { ref, provide, onMounted } from 'vue'
 
 use([
   PieChart,
@@ -34,40 +34,94 @@ use([
 // 這裡還不懂
 // provide(THEME_KEY, 'dark')
 
+const data = [
+  {
+    Year: '2022',
+    Rent: 72000,
+    Food: 48000,
+    Play: 80000,
+    Surplus: 50000,
+    Total: 72000 + 48000 + 80000
+  },
+  {
+    Year: '2023',
+    Rent: 96000,
+    Food: 72000,
+    Play: 50000,
+    Surplus: 80000,
+    Total: 96000 + 72000 + 50000
+  },
+  {
+    Year: '2024',
+    Rent: 120000,
+    Food: 108000,
+    Play: 30000,
+    Surplus: 30000,
+    Total: 120000 + 108000 + 30000
+  }
+]
+
 const barChartOption = ref({
   dataset: [
     {
       source: [
-        ['Year', 'Rent', 'Food', 'Entertainment', 'Surplus'],
-        ['2022', 72000, 48000, 80000, 50000],
-        ['2023', 96000, 72000, 50000, 80000],
-        ['2024', 120000, 108000, 30000, 30000]
+        ['Year', 'Rent', 'Food', 'Play', 'Surplus'],
+        ['2022', 72000 / 1000, 48000 / 1000, 80000 / 1000, 50000 / 1000],
+        ['2023', 96000 / 1000, 72000 / 1000, 50000 / 1000, 80000 / 1000],
+        ['2024', 120000 / 1000, 108000 / 1000, 30000 / 1000, 30000 / 1000]
       ]
     }
   ],
   series: [
-    { type: 'bar', xAxisIndex: 0, yAxisIndex: 0 },
-    { type: 'bar', xAxisIndex: 0, yAxisIndex: 0 },
-    { type: 'bar', xAxisIndex: 0, yAxisIndex: 0 },
-    { type: 'line', xAxisIndex: 0, yAxisIndex: 0 },
+    { type: 'bar' },
+    { type: 'bar' },
+    { type: 'bar' },
+    { type: 'line' },
 
-    { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 1, yAxisIndex: 1 },
-    { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 1, yAxisIndex: 1 },
-    { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 1, yAxisIndex: 1 }
+    {
+      type: 'bar',
+      seriesLayoutBy: 'row',
+      xAxisIndex: 1,
+      yAxisIndex: 1,
+      color: '#c7c7c7'
+    },
+    { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 1, yAxisIndex: 1, color: '#a19c9c' },
+    { type: 'bar', seriesLayoutBy: 'row', xAxisIndex: 1, yAxisIndex: 1, color: '#787575' }
   ],
   xAxis: [
     { type: 'category', gridIndex: 0 },
     { type: 'category', gridIndex: 1 }
   ],
-  yAxis: [{ gridIndex: 0 }, { gridIndex: 1 }],
-  grid: [{ bottom: '55%' }, { top: '55%' }],
+  yAxis: [
+    {
+      gridIndex: 0,
+      axisLabel: {
+        margin: 15
+      }
+    },
+    {
+      gridIndex: 1,
+      axisLabel: {
+        margin: 15
+      }
+    }
+  ],
+
+  grid: [
+    { bottom: '55%', left: '15%' },
+    { top: '55%', left: '15%' }
+  ],
 
   title: {
     text: 'Annual Expenditure',
-    left: 'center'
+    left: 0,
+    subtext: '單位(千元)'
   },
+
   legend: {
-    bottom: '0'
+    bottom: 0,
+    left: 'center',
+    itemWidth: 30
   },
   tooltip: {
     axisPointer: {
@@ -86,128 +140,127 @@ const barChartOption = ref({
     }
   }
 })
-
 const pieChartOption = ref({
-  title: {
-    text: 'Traffic Sources',
-    left: 'center'
-  },
-  tooltip: {
-    trigger: 'item',
-    formatter: '{a} <br/>{b} : {c} ({d}%)'
-  },
-  legend: {
-    orient: 'vertical',
-    left: 'left',
-    data: ['Direct', 'Email', 'Ad Networks', 'Video Ads', 'Search Engines']
-  },
-  series: [
-    {
-      name: 'Traffic Sources',
-      type: 'pie',
-      radius: '55%',
-      center: ['50%', '60%'],
-      data: [
-        { value: 335, name: 'Direct' },
-        { value: 310, name: 'Email' },
-        { value: 234, name: 'Ad Networks' },
-        { value: 135, name: 'Video Ads' },
-        { value: 1548, name: 'Search Engines' }
-      ],
-      emphasis: {
-        itemStyle: {
-          shadowBlur: 10,
-          shadowOffsetX: 0,
-          shadowColor: 'rgba(0, 0, 0, 0.5)'
-        }
-      }
-    }
-  ]
-})
-
-const test = ref({
   dataset: [
     {
       source: [
-        ['Product', 'Sales', 'Price', 'Year'],
-        ['Cake', 123, 32, 2011],
-        ['Cereal', 231, 14, 2011],
-        ['Tofu', 235, 5, 2011],
-        ['Dumpling', 341, 25, 2011],
-        ['Biscuit', 122, 29, 2011],
-        ['Cake', 143, 30, 2012],
-        ['Cereal', 201, 19, 2012],
-        ['Tofu', 255, 7, 2012],
-        ['Dumpling', 241, 27, 2012],
-        ['Biscuit', 102, 34, 2012],
-        ['Cake', 153, 28, 2013],
-        ['Cereal', 181, 21, 2013],
-        ['Tofu', 395, 4, 2013],
-        ['Dumpling', 281, 31, 2013],
-        ['Biscuit', 92, 39, 2013],
-        ['Cake', 223, 29, 2014],
-        ['Cereal', 211, 17, 2014],
-        ['Tofu', 345, 3, 2014],
-        ['Dumpling', 211, 35, 2014],
-        ['Biscuit', 72, 24, 2014]
+        ['Item', 'Expend', 'Year'],
+        ['Rent', 72000 / 200000, 2022],
+        ['Food', 48000 / 200000, 2022],
+        ['Play', 80000 / 200000, 2022],
+        ['Rent', 96000 / 218000, 2023],
+        ['Food', 72000 / 218000, 2023],
+        ['Play', 50000 / 218000, 2023],
+        ['Rent', 120000 / 258000, 2024],
+        ['Food', 108000 / 258000, 2024],
+        ['Play', 30000 / 258000, 2024]
       ]
     },
     {
       transform: {
         type: 'filter',
-        config: { dimension: 'Year', value: 2011 }
+        config: { dimension: 'Year', value: 2022 }
       }
     },
     {
       transform: {
         type: 'filter',
-        config: { dimension: 'Year', value: 2012 }
+        config: { dimension: 'Year', value: 2023 }
       }
     },
     {
       transform: {
         type: 'filter',
-        config: { dimension: 'Year', value: 2013 }
+        config: { dimension: 'Year', value: 2024 }
       }
     }
   ],
   series: [
     {
+      name: '嗨嗨',
       type: 'pie',
-      radius: 50,
-      center: ['25%', '50%'],
-      datasetIndex: 1
+      radius: [20, 40],
+      center: ['20%', '50%'],
+      datasetIndex: 1,
+      label: {
+        position: 'inside',
+        formatter: function (params) {
+          return params.value[1].toFixed(2) * 100 + '%'
+        }
+      }
     },
     {
       type: 'pie',
-      radius: 50,
+      radius: [20, 40],
       center: ['50%', '50%'],
-      datasetIndex: 2
+      datasetIndex: 2,
+      label: {
+        position: 'inside',
+        formatter: function (params) {
+          return params.value[1].toFixed(2) * 100 + '%'
+        }
+      }
     },
     {
       type: 'pie',
-      radius: 50,
-      center: ['75%', '50%'],
-      datasetIndex: 3
+      radius: [20, 40],
+      center: ['80%', '50%'],
+      datasetIndex: 3,
+      label: {
+        position: 'inside',
+        formatter: function (params) {
+          return params.value[1].toFixed(2) * 100 + '%'
+        }
+      }
     }
-  ]
+  ],
+  graphic: {
+    type: 'text',
+    left: 'center',
+    top: 'center',
+    style: {
+      text: '嗨嗨',
+      textAlign: 'center',
+      fill: '#000',
+      fontSize: 20,
+      fontWeight: 'bold'
+    }
+  }
+})
+
+function handleResize() {
+  barChartOption.value.title.left = window.innerWidth > 800 ? 'center' : 0
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
 })
 </script>
 
 <template>
-  <v-chart class="barChart" :option="barChartOption" autoresize />
-  <v-chart class="pieChart" :option="pieChartOption" autoresize />
-  <v-chart class="pieChart" :option="test" autoresize />
+  <div class="bigbox">
+    <v-chart class="barChart" :option="barChartOption" autoresize />
+    <v-chart class="pieChart" :option="pieChartOption" autoresize />
+  </div>
 </template>
 
 <style scoped>
+.bigbox {
+  width: 100vw;
+  overflow-x: hidden;
+}
 .barChart {
-  width: 90vw;
-  height: 70vh;
-  margin-bottom: 100px;
+  width: 90%;
+  height: 100vw;
+  margin: 0 auto;
+  @media screen and (min-width: 800px) {
+    width: 50%;
+    height: 50vw;
+  }
 }
 .pieChart {
-  width: 90vw;
-  height: 50vh;
+  width: 90%;
+  height: 50vw;
+  margin: 0 auto;
 }
 </style>
